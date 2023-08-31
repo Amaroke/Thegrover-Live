38b52bff25b676d9d4809d5dcc8c844634592504
Amaroke <hugo.amaroke@gmail.com>
2023-08-31 12:29:09 +0200

    update api

--- backend/api/isThegroverLive.js
+++ backend/api/isThegroverLive.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require("cors");
 dotenv.config();
 const clientId = 'aoqq5yq3nokymtw9zpup1f5i270y8w';
 const clientSecret = process.env.TWITCH_CLIENT_SECRET;
@@ -7,13 +8,9 @@ const channelName = 'thegrover_';
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET');
-    next();
-});
+app.use(cors());
 
-app.get('/isTheGroverLive', async (req, res) => {
+app.get('/isThegroverLive', async (req, res) => {
     try {
         console.log(clientId, clientSecret, channelName)
         const authUrl = `https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`;
@@ -46,8 +43,12 @@ app.get('/isTheGroverLive', async (req, res) => {
     }
 });
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    console.log(`L'API est en cours d'exécution sur le port ${port}`);
+app.get('*', (req, res) => {
+    res.status(404).json({ error: 'Resource not found' });
 });
+
+app.listen(5000, () => {
+    console.log('Running on port 5000.');
+});
+
+module.exports = app;
