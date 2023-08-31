b18fe89b7c72e4fab082846c243f3841dab3109e
Amaroke <hugo.amaroke@gmail.com>
2023-08-31 12:35:21 +0200

    Création de l'extension

---
+++ background.js
@@ -0,0 +1,24 @@
+function checkIfTheGroverIsLive() {
+    fetch('https://thegrover-live.vercel.app/isThegroverLive', {
+        method: 'GET',
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data === true) {
+                chrome.browserAction.setIcon({ path: "images/icon_online.png" });
+            } else {
+                chrome.browserAction.setIcon({ path: "images/icon_offline.png" });
+            }
+        })
+        .catch(error => {
+            console.error('Erreur lors de la vérification de l\'état de The Grover:', error);
+        });
+}
+
+function getThegroverLive() {
+    checkIfTheGroverIsLive();
+
+    setInterval(checkIfTheGroverIsLive, 5 * 60 * 1000);
+}
+
+getThegroverLive();
